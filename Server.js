287fbeaa3c7756cfa4e0c31f62c8127609d12a1b
chitwan <122033850+chitwankaur20@users.noreply.github.com>
2025-10-23 00:14:23 +0530

    fourth commit

--- Server.js
+++ Server.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const app = require('./src/app.js')
+const app = require('./Backend/src/app.js')
 
 app.listen(3000, ()=> {
     console.log('Server is running on http://localhost:3000')
